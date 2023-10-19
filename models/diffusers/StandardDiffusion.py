3e41d2030713e787c713c938f93b2caed0ff78eb
LeonardoBerti07 <leonardo.berti07@gmail.com>
2023-10-19 22:22:55 +0200

    Standard Diffusion model (not finished)

---
+++ models/diffusers/StandardDiffusion.py
@@ -0,0 +1,62 @@
+import math
+
+import torch
+from models.diffusers.DiffusionModel import DiffusionAB
+import constants as cst
+from constants import LearningHyperParameter
+from torch import nn
+from utils import compute_mean_tilde_t
+
+
+class Standard_Diffusion(nn.Module, DiffusionAB):
+    """An abstract class for loss functions."""
+
+    def __init__(self, config, alphas_dash, betas):
+        self.dropout = config.HYPER_PARAMETERS[LearningHyperParameter.DROPOUT]
+        self.batch_size = config.HYPER_PARAMETERS[LearningHyperParameter.BATCH_SIZE]
+        self.diffusion_steps = config.HYPER_PARAMETERS[LearningHyperParameter.DIFFUSION_STEPS]
+        self.lambda_ = config.HYPER_PARAMETERS[LearningHyperParameter.LAMBDA]
+        self.K = config.HYPER_PARAMETERS[LearningHyperParameter.MASKED_WINDOW_SIZE]
+        if config.IS_AUGMENTATION:
+            self.features_size = config.HYPER_PARAMETERS[LearningHyperParameter.AUGMENT_DIM]
+        else:
+            self.features_size = cst.LEN_EVENT
+        self.NN = nn.Transformer(d_model=self.features_size, nhead=8, num_encoder_layers=2, num_decoder_layers=2, dim_feedforward=2048, dropout=self.dropout, activation='relu', batch_first=True)
+        self.alphas_dash = alphas_dash
+        self.betas = betas
+        self.losses = []
+        self.v = nn.Parameter(torch.randn(self.features_size))
+
+
+    def forward(self, x_T, eps_true):
+        return self.reverse(x_T, eps_true)
+
+
+    def reverse_reparameterized(self, x_T, eps_true):
+        x_t = x_T
+        losses = []
+        for t in range(1, self.diffusion_steps):
+            beta_t = self.betas[t]
+            alpha_t = 1 - beta_t
+            alpha_dash_t = self.alphas_dash[t]
+            beta_tilde_t = (1 - self.alphas_dash[t-1]) / (1 - self.alphas_dash[t]) * beta_t
+            eps_t = self.NN(x_t, t)
+            z = torch.distributions.Normal(0, 1).sample(x_t.shape)
+            sigma_t = math.exp(self.v*math.log(beta_t) + (1-self.v)*math.log(beta_tilde_t))       #formula taken from IDDPM paper
+            x_t = 1 / math.sqrt(alpha_t) * (x_t[:] - (beta_t / math.sqrt(1 - alpha_dash_t)) * eps_t) + sigma_t * z
+            L_t = self.loss_step(eps_t, eps_true)
+            losses.append(L_t)
+        self.losses.append(losses)
+        return x_t
+
+
+    def loss_step_reparameterized(self, eps_t, eps_true):
+        return torch.norm(eps_t - eps_true, p=2)
+
+
+    def loss(self, true: torch.Tensor, recon: torch.Tensor, **kwargs) -> torch.Tensor:
+        """Computes the loss taken from IDDPM."""
+        L_simple = self.losses[-1]
+        L_vlb = ...
+        L_hybrid = L_simple + self.lambda_*L_vlb
+        return L_hybrid
