e7e179481fec9972b40a23f83f2e3c5e50e8ec64
MarcoGuarino <guarino.1895383@studenti.uniroma1.it>
2023-10-25 18:12:42 +0200

    jsd and ks (approaches for evaluation)

---
+++ evaluation/evaluation_utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.stats import entropy
+import scipy.stats as stats
+
+######################### Jensen-Shannon Divergence #########################
+# It is used to measure the similarity between two probability distributions #
+
+class JSDCalculator:
+    def __init__(self, historical_data, generated_data, bins=10):
+        self.p_1 = np.mean(historical_data, axis=0)
+        self.q_1 = np.mean(generated_data, axis=0)
+        self.bins = bins
+
+    def jsd(self):
+        # calculate the average distribution
+        m = 0.5 * (self.p_1 + self.q_1)
+        # calculate the divergence of Kullback-Leibler between each distribution and the average
+        dkl_p = entropy(self.p_1, m, base=2)
+        dkl_q = entropy(self.q_1, m, base=2)
+        # Calculate the JSD
+        jsd = 0.5 * (dkl_p + dkl_q)
+        return jsd
+
+    def calculate_jsd(self):
+        # Calcola la JSD tra le distribuzioni empiriche
+        jsd = self.jsd()
+        print(f"JSD tra le distribuzioni empiriche: {jsd:.3f}")
+
+
+######################### Kolmogorov-Smironov statistic (test) #########################
+# It is used to measure the similarity between two probability distributions #
+
+class KSCalculator:
+    def __init__(self, historical_data, generated_data):
+        self.p_1 = np.mean(historical_data, axis=0)
+        self.q_1 = np.mean(generated_data, axis=0)
+
+    def calculate_ks(self):
+        ks_stat, p_value = stats.ks_2samp(self.p_1, self.q_1)
+        print(f"Test statistic is {ks_stat:.4f}")
+        print(f"Test value si{p_value:.4f}")
+        if p_value < 0.05:
+            print("We reject the null hypothesis that the two datasets come from the same distribution")
+        else:
+            print("We do not reject the null hypothesis that the two datasets come from the same distribution")
+        return ks_stat, p_value
+    
+
+
+'''
+JSD is a symmetric and smoothed version of Kullback-Leibler Divergence (KLD) that measures the similarity between two probability distributions. It is defined as the square root of the Jensen-Shannon Divergence,
+which is a smoothed version of KLD. JSD is commonly used in probability theory and information theory to compare probability distributions.
+
+KS statistic, on the other hand, is a non-parametric test that compares two probability distributions by measuring the maximum difference between their cumulative distribution functions (CDFs).
+
+It is based on the empirical distribution functions of the two datasets and is used to test whether two samples are drawn from the same distribution.
+'''
